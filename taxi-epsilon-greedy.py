bc5417f796b4c9d96d502de7cb9cfb0885b82ead
Lewis Neal <l_neal_@hotmail.co.uk>
2020-03-07 13:51:59 +0000

    Add epsilon greedy for taxi

---
+++ taxi-epsilon-greedy.py
@@ -0,0 +1,64 @@
+import gym, datetime
+import numpy as np
+from epsilon_greedy import EpsilonGreedy
+env = gym.make('Taxi-v3')
+
+def update_q_function(current_state, next_state, action, reward):
+    q_function[current_state, action] = q_function[current_state, action] + learning_rate * (reward + discount_factor * np.max(q_function[next_state, :]) - q_function[current_state, action])
+
+def reset_q_function():
+    return np.zeros([env.observation_space.n, env.action_space.n])
+
+# Parameters
+learning_rate = 0.1
+discount_factor = 0.9
+episodes = 2000
+epsilon = 1
+epsilon_discount_factor = 0.9999
+steps = 1000
+
+log_dir = 'data/taxi'
+filepath = log_dir + '/taxi-epsilon-greedy'
+date_string = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+
+epsilon_greedy = EpsilonGreedy(epsilon, epsilon_discount_factor)
+
+cumulative_reward = 0
+logs = []
+q_function = reset_q_function()
+
+for i_episode in range(episodes):
+    epsilon_greedy.reset()
+    current_state = env.reset()
+    cumulative_reward = 0
+    for t in range(steps):
+        action = epsilon_greedy.select_action(current_state, q_function, env)
+        next_state, reward, done, info = env.step(action)
+        update_q_function(current_state, next_state, action, reward)
+        current_state = next_state
+        cumulative_reward += reward
+        if done:
+            break
+        logs.append([i_episode, t, reward, cumulative_reward])
+
+cumulative_reward = 0
+current_state = env.reset()
+env.render()
+
+for i in range(10000):
+    action = np.argmax(q_function[current_state,:])
+    next_state, reward, done, info = env.step(action)
+    current_state = next_state
+    cumulative_reward += reward
+    env.render()
+    if done:
+        break
+print("Episode finished after {} timesteps".format(i+1))
+print("Cumulative reward at end = " + str(cumulative_reward))
+env.close()
+
+np.savetxt(filepath + '-q-function-' + date_string, q_function, delimiter=',')
+
+# data is logged in the format: episode, step, reward, cumulative reward
+np.savetxt(filepath + date_string, logs, delimiter=',')
+
