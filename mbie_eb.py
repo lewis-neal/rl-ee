28867eea010f7ad7a794b7328fa3138aea9500d4
Lewis Neal <l_neal_@hotmail.co.uk>
2020-03-07 14:50:03 +0000

    Add mbie-eb action selector + taxi for mbie-eb

---
+++ mbie_eb.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class MBIE_EB:
+    def __init__(self, beta, state_dim, action_dim, discount_factor):
+        self.__beta = beta
+        self.__state_dim = state_dim
+        self.__action_dim = action_dim
+        self.reset()
+        self.__bonus_func = np.vectorize(self.__get_bonus)
+        self.__discount_factor = discount_factor
+
+    def __update_count(self, state, action):
+        self.__counts[state, action] += 1
+
+    def __get_bonuses(self):
+        return self.__bonus_func(self.__counts)
+
+    def __get_bonus(self, count):
+        # as per: An Analysis of Model-Based Interval Estimation for Markov Decision Processes
+        if count == 0:
+            return 1 / (1 - self.__discount_factor)
+        return self.__beta * 1/(count ** 0.5) 
+
+    def reset(self):
+        self.__counts = np.zeros([self.__state_dim, self.__action_dim])
+
+    def select_action(self, state, q_function):
+        with_bonuses = np.add(q_function, self.__get_bonuses())
+        action = np.argmax(q_function[state,:]) 
+        self.__update_count(state, action)
+        return action
+
---
+++ taxi-mbie-eb.py
@@ -0,0 +1,62 @@
+import gym, datetime
+import numpy as np
+from mbie_eb import MBIE_EB
+env = gym.make('Taxi-v3')
+
+# Parameters
+learning_rate = 0.1
+discount_factor = 0.9
+episodes = 2000
+beta = 0.05
+steps = 1000
+
+log_dir = 'data/taxi'
+filepath = log_dir + '/taxi-mbie-eb'
+date_string = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+
+action_selector = MBIE_EB(beta, env.observation_space.n, env.action_space.n, discount_factor)
+
+def update_q_function(current_state, next_state, action, reward):
+    q_function[current_state, action] = q_function[current_state, action] + learning_rate * (reward + discount_factor * np.max(q_function[next_state, :]) - q_function[current_state, action])
+
+def reset_q_function():
+    return np.zeros([env.observation_space.n, env.action_space.n])
+
+cumulative_reward = 0
+logs = []
+q_function = reset_q_function()
+
+for i_episode in range(episodes):
+    current_state = env.reset()
+    cumulative_reward = 0
+    for t in range(steps):
+        action = action_selector.select_action(current_state, q_function)
+        next_state, reward, done, info = env.step(action)
+        update_q_function(current_state, next_state, action, reward)
+        current_state = next_state
+        cumulative_reward += reward
+        if done:
+            break
+        logs.append([i_episode, t, reward, cumulative_reward])
+
+cumulative_reward = 0
+current_state = env.reset()
+env.render()
+
+for i in range(10000):
+    action = np.argmax(q_function[current_state,:])
+    next_state, reward, done, info = env.step(action)
+    current_state = next_state
+    cumulative_reward += reward
+    env.render()
+    if done:
+        break
+print("Episode finished after {} timesteps".format(i+1))
+print("Cumulative reward at end = " + str(cumulative_reward))
+env.close()
+
+np.savetxt(filepath + '-q-function-' + date_string, q_function, delimiter=',')
+
+# data is logged in the format: episode, step, reward, cumulative reward
+np.savetxt(filepath + date_string, logs, delimiter=',')
+
