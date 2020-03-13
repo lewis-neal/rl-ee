79235c1b0fea85e253fd46a764282247d3c1a751
Lewis Neal <l_neal_@hotmail.co.uk>
2020-03-13 23:14:30 +0000

    WIP Add Boltzmann

---
+++ boltzmann.py
@@ -0,0 +1,57 @@
+import numpy as np
+from math import exp
+
+class Boltzmann:
+    def __init__(self, temperature):
+        self.__temperature = temperature
+        self.__original_temp = temperature
+
+    def select_action(self, state, q_function, env):
+        probabilities = self.__get_action_probabilities(state, q_function)
+        self.__update_temperature()
+        num = np.random.uniform()
+        for action in probabilities:
+            if num < action[0]:
+                return int(action[1])
+
+    def __get_action_probabilities(self, state, q_function):
+        means, total = self.__get_means(state, q_function)   
+        actions = []
+        last = 0
+        for value in means:
+            action = np.zeros(2)
+            prob = last + (value[0] / total)
+            action[0] = prob
+            action[1] = value[1]
+            actions.append(action)
+            last = prob
+        return actions
+
+
+
+    def __get_means(self, state, q_function):
+        means = []
+        q_values = q_function[state,:]
+        q_values = np.sort(q_values)[::-1]
+        total = 0
+        pointer = 0
+        for value in q_values:
+            action = np.zeros(2)
+            mean = self.__get_mean(value)
+            total += mean
+            action[0] = mean
+            action[1] = pointer
+            means.append(action)
+            pointer += 1
+        return means, total
+
+    def __get_mean(self, q_value):
+        return exp(q_value / self.__temperature)
+
+    def __update_temperature(self):
+        if self.__temperature == 1:
+            return
+        self.__temperature -= 1
+
+    def reset(self):
+       self.__temperature = self.__original_temp 
