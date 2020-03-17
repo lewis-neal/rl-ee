eef73340149c2070b6a606289a85580eb5ec9994
Lewis Neal <l_neal_@hotmail.co.uk>
2020-03-17 13:16:34 +0000

    Update benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,14 +1,19 @@
 import gym
 import numpy as np
-env = gym.make('Taxi-v3')
+from discrete import Discrete
+from env_wrapper import EnvWrapper
+env = gym.make('CartPole-v1')
 
 # Parameters
-episodes = 10000
+episodes = 100
 steps = 1000
 episode_reward = 0
 total_reward = 0
+num_states = [10, 0, 10, 0]
 
-q_function = np.loadtxt('data/taxi/taxi-mbie-eb-q-function-2020-03-07_16:43:44', delimiter=',')
+q_function = np.loadtxt('data/cart-pole/epsilon-greedy2020-03-17_13:08:34-q-function.csv', delimiter=',')
+discrete = Discrete(num_states, env)
+env = EnvWrapper(env, num_states, discrete)
 
 for ep in range(episodes):
     current_state = env.reset()
@@ -18,7 +23,9 @@ for ep in range(episodes):
         next_state, reward, done, info = env.step(action)
         current_state = next_state
         episode_reward += reward
+        env.render()
         if done:
+            print(episode_reward)
             break
     total_reward += episode_reward
 average_reward = total_reward / episodes
