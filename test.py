196462c5e6412cbca84234a2d04bebfac2a73e34
Lewis Neal <l_neal_@hotmail.co.uk>
2020-03-18 19:30:43 +0000

    Remove redundant files

--- test.py
+++ test.py
@@ -5,15 +5,12 @@ from epsilon_greedy import EpsilonGreedy
 from mbie_eb import MBIE_EB
 from boltzmann import Boltzmann
 from logger import Logger
-from env_wrapper import EnvWrapper
-from discrete import Discrete
-from action_space_wrapper import ActionWrapper
 from env_handler import EnvHandler
 
 # Parameters
 learning_rate = 0.1
 discount_factor = 0.9
-episodes = 100
+episodes = 1000
 epsilon = 1
 epsilon_discount_factor = 0.9999
 steps = 200
@@ -26,20 +23,15 @@ date_string = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
 filepath = log_dir + '/epsilon-greedy' + date_string
 env_handler = EnvHandler()
 env = env_handler.get_env('Taxi-v3')
-# add discrete for non-discrete state spaces
-discrete = Discrete(num_states, env)
-# add action_wrapper for non-discrete action spaces
-#action_wrapper = ActionWrapper(env.action_space.high[0], env.action_space.low[0], 100)
-total_states = discrete.get_total_states()
 
 q_function = Q(env.get_total_states(), env.get_total_actions(), learning_rate, discount_factor)
-action_selector = EpsilonGreedy(epsilon, epsilon_discount_factor)
-#action_selector = MBIE_EB(beta, total_states, env.action_space.n, discount_factor)
-#action_selector = Boltzmann(temperature)
+#action_selector = EpsilonGreedy(epsilon, epsilon_discount_factor)
+#action_selector = MBIE_EB(beta, env.get_total_states(), env.get_total_actions(), discount_factor)
+action_selector = Boltzmann(temperature)
 logger = Logger(episodes, 1)
 
 agent = Agent(env, q_function, action_selector, logger)
 
 agent.train(steps, episodes)
 
-agent.solve(steps, filepath, False)
+agent.solve(steps, filepath, True)
