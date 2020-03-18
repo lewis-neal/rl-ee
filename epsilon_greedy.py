4caa7f6e4b0dca08b845d52195e1fd60240aa039
Lewis Neal <l_neal_@hotmail.co.uk>
2020-03-18 19:35:05 +0000

    Remove redundant code from q_function and epsilon_greedy

--- epsilon_greedy.py
+++ epsilon_greedy.py
@@ -1,21 +1,15 @@
 import numpy as np
-from action_space_wrapper import ActionWrapper
 
 class EpsilonGreedy:
-    def __init__(self, epsilon, epsilon_discount_factor, action_wrapper=None):
+    def __init__(self, epsilon, epsilon_discount_factor):
         self.__epsilon = epsilon
         self.__original_epsilon = epsilon
         self.__epsilon_discount_factor = epsilon_discount_factor
-        self.__discretise = action_wrapper == None
-        if not self.__discretise:
-            self.__action_wrapper = action_wrapper
 
     def select_action(self, current_state, q_function, env):
         if np.random.uniform() > self.__epsilon:
             self.__update_epsilon()
             action = q_function.get_best_action(current_state)
-            if not self.__discretise:
-                return self.__action_wrapper.undiscretise(action)
             return action
         self.__update_epsilon()
         return env.get_random_action()
