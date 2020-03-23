492dc8d421bccc3da38228ac352c492e8eb8a48d
Lewis Neal <l_neal_@hotmail.co.uk>
2020-03-23 19:06:46 +0000

    Add ucb-1 action selection strategy

--- test.py
+++ test.py
@@ -6,34 +6,44 @@ from mbie_eb import MBIE_EB
 from boltzmann import Boltzmann
 from logger import Logger
 from env_handler import EnvHandler
+from ucb_1 import UCB_1
 
 # Parameters
 learning_rate = 0.1
 discount_factor = 0.9
-episodes = 1000
+episodes = 5000
 epsilon = 1
 epsilon_discount_factor = 0.9999
 steps = 200
 beta = 0.05
 temperature = 1000000
 env_name = 'Taxi-v3'
+seed = 101
+c = 10
 
 log_dir = 'data/' + env_name
 date_string = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
 filepath = log_dir + '/epsilon-greedy' + date_string
 env_handler = EnvHandler()
 env = env_handler.get_env(env_name)
+env.seed(seed)
 
 q_function = Q(env.get_total_states(), env.get_total_actions(), learning_rate, discount_factor)
 #action_selector = EpsilonGreedy(epsilon, epsilon_discount_factor)
-action_selector = MBIE_EB(beta, env.get_total_states(), env.get_total_actions(), discount_factor)
+#action_selector = MBIE_EB(beta, env.get_total_states(), env.get_total_actions(), discount_factor)
 #action_selector = Boltzmann(temperature)
+action_selector = UCB_1(c, env.get_total_states(), env.get_total_actions(), discount_factor)
 logger = Logger(episodes)
 
 agent = Agent(env, q_function, action_selector, logger)
 
-agent.train(steps, episodes)
+agent.train(steps, episodes, filepath)
 
-agent.solve(steps, filepath, False)
+env.seed(seed)
+agent.solve(steps, True)
+agent.solve(steps, True)
+env.seed(seed)
+agent.solve(steps, True)
+agent.solve(steps, True)
 
 env.close()
---
+++ ucb_1.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class UCB_1:
+    def __init__(self, c, state_dim, action_dim, discount_factor):
+        self.__c = c
+        self.__state_dim = state_dim
+        self.__action_dim = action_dim
+        self.reset()
+        self.__bonus_func = np.vectorize(self.__get_bonus)
+        self.__discount_factor = discount_factor
+
+    def __update_count(self, state, action):
+        self.__counts[state, action] += 1
+
+    def __get_bonuses(self, state):
+        total = sum(self.__counts[state,:])
+        return self.__bonus_func(self.__counts[state,:], total)
+
+    def __get_bonus(self, count, total):
+        # as per: An Analysis of Model-Based Interval Estimation for Markov Decision Processes
+        if count == 0:
+            return 1 / (1 - self.__discount_factor)
+        return self.__c * ((2 * np.log(total) / count) ** 0.5)
+
+    def reset(self):
+        self.__counts = np.zeros([self.__state_dim, self.__action_dim])
+
+    def select_action(self, state, q_function, env):
+        action = q_function.get_best_action(state, self.__get_bonuses(state))
+        self.__update_count(state, action)
+        return action
+
