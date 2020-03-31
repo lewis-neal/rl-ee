66d55cb11ca91de815c7cbf1cc6a3d264cc61dde
Lewis Neal <l_neal_@hotmail.co.uk>
2020-03-31 13:42:16 +0100

    Update train.py to take csv of parameters and envs

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import sys, csv, os
 import numpy as np
 from env_handler import EnvHandler
 from q_function import Q
@@ -6,32 +7,52 @@ from logger import Logger
 from action_selectors.mbie_eb import MBIE_EB
 from action_selectors.epsilon_greedy import EpsilonGreedy
 from action_selectors.boltzmann import Boltzmann
+from action_selectors.controlability import Controlability
+from action_selectors.ucb_1 import UCB_1
+from action_selectors.vdbe import VDBE
 
 env_handler = EnvHandler()
+# args = [action selector name, base dir, path to params file]
+args = sys.argv[1:]
 
-episodes = 1000
+episodes = 1
 steps = 200
 learning_rate = 0.1
 discount_factor = 0.9
-env_names = [['Acrobot-v1', 1], ['CartPole-v1', 1], ['MountainCar-v0', 1], ['Pendulum-v0', 1], \
-['Copy-v0', 1], ['DuplicatedInput-v0', 1], ['RepeatCopy-v0', 1], ['Reverse-v0', 1], ['ReversedAddition-v0', 1], ['ReversedAddition3-v0', 1], \
-['Blackjack-v0', 1], ['Roulette-v0', 1], ['FrozenLake-v0', 1], ['FrozenLake8x8-v0', 1], ['NChain-v0', 1], ['Taxi-v3', 1], ['GuessingGame-v0', 1], ['HotterColder-v0', 1]]
 epsilon = 1
 seeds = range(20, 30)
-action_selector_name = 'random'
+action_selector_name = args[0]
+base_dir = args[1] + '/data/'
+params_path = args[2]
+with open(params_path, newline='') as csvfile:
+    env_names = list(csv.reader(csvfile))
 
-for seed in seeds:
-    for env_name, val in env_names:
+for env_name, val in env_names:
+    val = float(val)
+    for seed in seeds:
         print(env_name)
         print(seed)
         env = env_handler.get_env(env_name)
         env.seed(seed)
         q_function = Q(env.get_total_states(), env.get_total_actions(), learning_rate, discount_factor)
-        log_dir = 'data/' + env_name + '/' + action_selector_name
-        action_selector = EpsilonGreedy(epsilon, val, seed)#action_selector = Boltzmann(val, seed)#MBIE_EB(val, env.get_total_states(), env.get_total_actions(), discount_factor)
+        log_dir = base_dir + env_name + '/' + action_selector_name + '/' + 'final'
+        os.makedirs(log_dir + '/q_function', exist_ok=True)
+        os.makedirs(log_dir + '/training-data', exist_ok=True)
+        if action_selector_name == 'epsilon-greedy' or action_selector_name == 'random':
+            action_selector = EpsilonGreedy(epsilon, val, seed)
+        elif action_selector_name == 'boltzmann':
+            action_selector = Boltzmann(val, seed)
+        elif action_selector_name == 'ucb-1':
+            action_selector = UCB_1(val, env.get_total_states(), env.get_total_actions())
+        elif action_selector_name == 'vdbe':
+            action_selector = VDBE(env.get_total_states(), val, 1 / env.get_total_actions(), learning_rate, seed)
+        elif action_selector_name == 'mbie-eb':
+            action_selector = MBIE_EB(val, env.get_total_states(), env.get_total_actions(), discount_factor)
+        elif action_selector_name == 'controlability':
+            action_selector = Controlability(val, env.get_total_states(), env.get_total_actions(), learning_rate, omega)
         q_function.reset()
         logger = Logger(episodes)
         agent = Agent(env, q_function, action_selector, logger)
-        filename = action_selector_name + '-seed-' + str(seed)
+        filename = str(seed)
         agent.train(steps, episodes, log_dir, filename)
 print('Done')
